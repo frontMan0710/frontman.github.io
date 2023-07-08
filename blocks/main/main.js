d48fada43623364a5b23396c3dc5d18cab70c54e
Bodya <mr.cdrmn277@gmail.com>
2023-07-08 13:32:33 +0300

    add classes to mobile - title and text, added slider

---
+++ blocks/main/main.js
@@ -0,0 +1,43 @@
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i)
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i)
+    },
+    check: function () {
+        return (
+        isMobile.Android() || isMobile.Windows()
+        )
+    }
+}
+var titles = document.querySelectorAll("h2")
+var textBlocks = [
+    document.querySelectorAll(".body-text-wrapper__second-body-text"),
+    document.querySelectorAll(".body-text-wrapper__fourth-body-text"),
+    document.querySelectorAll(".body-text-wrapper__fifth-body-text"),
+    document.querySelectorAll(".body-text-wrapper__seventh-body-text")
+]
+
+
+if (isMobile.check()) {
+    titles.forEach(function (title) {
+        title.classList.add("mobile-sub-title");
+    });
+    textBlocks.forEach(function(block) {
+        block.forEach(function(element) {
+            element.classList.add("mobile-body-text");
+        });
+    });
+} else {
+    titles.forEach(function(title) {
+        title.classList.add("sub-title");
+    });
+    textBlocks.forEach(function(block) {
+        block.forEach(function(element) {
+            element.classList.add("body-text");
+        });
+    });
+}
+
---
+++ blocks/main/sliders.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper('.card-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
